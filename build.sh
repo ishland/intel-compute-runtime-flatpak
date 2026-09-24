e9d2be572bfdc60c0c40387662f807673f62f42a
ishland <30385023+ishland@users.noreply.github.com>
2026-09-24 17:11:44 +0800

    new: init gha

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ mkdir bundles/
 
 VERSION=26.08
 
-flatpak-builder --repo=build --install-deps-from=flathub --user --force-clean build --install ../org.freedesktop.Platform.GL.OpenCLIntel.yaml || exit 1
+flatpak-builder --repo=repo --install-deps-from=flathub --user --force-clean build --install ../org.freedesktop.Platform.GL.OpenCLIntel.yaml || exit 1
 echo generating normal bundle
 flatpak build-bundle repo bundles/org.freedesktop.Platform.GL.OpenCLIntel.flatpak org.freedesktop.Platform.GL.OpenCLIntel --runtime-repo=https://dl.flathub.org/repo/flathub.flatpakrepo --runtime "$VERSION" || exit 1
 echo generating debug bundle
